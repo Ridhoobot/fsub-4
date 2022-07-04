579613786dd9e3ceee9728a321233fa5e5159263
Ridho X Apo Nnetawin <106302889+Ridhoobot@users.noreply.github.com>
2022-07-04 19:32:18 +0800

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -14,7 +14,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://t.me/idooo8'>Klik Disini</a></b>\n",
+            text=f"<b>Tentang Bot ini:\n\n • Owner: @idooo8\n • Channel: @{CHANNEL}\n • Pemilik Repo: <a href='https://t.me/idooo5'>Ridoo</a></b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton(" ᴛᴜᴛᴜᴘ ", callback_data="close")]]
